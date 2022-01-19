bb7952c426647a3b2a0f399c43cf5ebd41cf55f7
yusunghyun <ysh1513@naver.com>
2022-01-20 00:22:31 +0900

    ➕ install mui, react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Button } from '@mui/material';
 
-function App() {
+const queryClient = new QueryClient();
+
+const App = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Button variant='contained'>Hello World</Button>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
