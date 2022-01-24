7ab32c1e3d9796471e19f7d9b67b8175649ae443
yusunghyun <ysh1513@naver.com>
2022-01-25 00:26:57 +0900

    ✨   ch4-4 caver.js(write)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { Button } from '@mui/material';
-import Caver from 'caver-js';
+import Caver, { KeyringContainer } from 'caver-js';
 import {
   COUNT_ABI,
   COUNT_CONTRACT_ADDRESS,
-  KAIKAS_ADDRESS,
+  KAIKAS_PRIVATE_KEY,
   klaytnApiUrl,
   option,
 } from './constant';
@@ -27,6 +27,26 @@ const getBalance = (address: string) => {
   });
 };
 
+const setCount = async (newCount: number) => {
+  try {
+    const caverWallet = caver.wallet as KeyringContainer;
+    // 사용할 account 설정
+    const deployer =
+      caverWallet.keyring.createFromPrivateKey(KAIKAS_PRIVATE_KEY);
+    caverWallet.add(deployer);
+    // 스마트 컨트렉트 실행 트랜잭션 날리기
+    // 결과 확인
+
+    const receipt = await CountContract.methods.setCount(newCount).send({
+      from: deployer.address,
+      gas: '0x4bfd200',
+    });
+    console.log('receipt,', receipt);
+  } catch (error) {
+    console.log('error : ', error);
+  }
+};
+
 // 1 smart contract 배포 주소 파악(가져오기)
 // 2 caver.js 이용해서 스마트 컨트랙트 연동하기
 // 3 가져온 스마트 컨트랙트 실행 결과(데이터) 웹에 표현하기
@@ -39,7 +59,8 @@ const App = (): JSX.Element => {
 
   useEffect(() => {
     readCount();
-    getBalance(KAIKAS_ADDRESS);
+    getBalance(KAIKAS_PRIVATE_KEY);
+    setCount(3);
   }, []);
 
   return <Button variant='contained'>Hello World</Button>;
