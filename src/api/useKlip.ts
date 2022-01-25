4ccbfc97d697d1075946b1cde8957415313af5e9
yusunghyun <ysh1513@naver.com>
2022-01-25 22:50:25 +0900

    ✨ klip api for mobile

--- src/api/useKlip.ts
+++ src/api/useKlip.ts
@@ -1,1 +1,24 @@
+import axios, { AxiosResponse } from 'axios';
+const KLIP_POST_API_URL = 'https://a2a-api.klipwallet.com/v2/a2a/prepare';
+const KLIP_GET_API_URL =
+  'https://a2a-api.klipwallet.com/v2/a2a/result?request_key=';
+
+export const getAddress = async () => {
+  const result: AxiosResponse = await axios.post(KLIP_POST_API_URL, {
+    bapp: {
+      name: 'KLAY_WALLET',
+    },
+    type: 'auth',
+  });
+
+  const { request_key } = result.data;
+  let timerId = setInterval(async () => {
+    const getResult = await axios.get(KLIP_GET_API_URL + request_key);
+    if (getResult.data.result) {
+      console.log('result : ', getResult.data.result);
+      clearInterval(timerId);
+    }
+  }, 1000);
+};
+
 export const useKlip = () => {};
