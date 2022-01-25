d0e0352fb8ecd803c08fbcd37381708d71642c2b
yusunghyun <ysh1513@naver.com>
2022-01-25 23:50:32 +0900

    ✨ klip api로 스마트컨트랙트 쏘기

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { Button } from '@mui/material';
 import QRCode from 'qrcode.react';
-import { getAddress } from './api/useKlip';
+import { getAddress, setCount } from './api/useKlip';
 
 const DEFAULT_QR_CODE = 'DEFAULT';
 
@@ -12,11 +12,17 @@ const App = (): JSX.Element => {
   const onClickGetAddress = () => {
     getAddress(setQrValue);
   };
+  const onClickSetCount = async () => {
+    await setCount(2000, setQrValue);
+  };
 
   return (
     <>
       <Button variant='contained' onClick={onClickGetAddress}>
         클립 주소 가져오기
+      </Button>
+      <Button variant='contained' onClick={onClickSetCount}>
+        카운트 변경
       </Button>
       <QRCode value={qrValue} />
     </>
--- src/api/useKlip.ts
+++ src/api/useKlip.ts
@@ -1,8 +1,53 @@
 import axios, { AxiosResponse } from 'axios';
 import { Dispatch, SetStateAction } from 'react';
+import { COUNT_CONTRACT_ADDRESS_MAINNET } from '../constants';
 const KLIP_POST_API_URL = 'https://a2a-api.klipwallet.com/v2/a2a/prepare';
 const KLIP_GET_API_URL =
   'https://a2a-api.klipwallet.com/v2/a2a/result?request_key=';
+
+export const setCount = async (
+  count: number,
+  setQrValue: Dispatch<SetStateAction<string>>,
+) => {
+  const result: AxiosResponse = await axios.post(KLIP_POST_API_URL, {
+    bapp: {
+      name: 'KLAY_WALLET',
+    },
+    type: 'execute_contract',
+    transaction: {
+      // from:
+      to: COUNT_CONTRACT_ADDRESS_MAINNET,
+      value: '0',
+      abi: JSON.stringify({
+        constant: false,
+        inputs: [
+          {
+            name: '_count',
+            type: 'uint256',
+          },
+        ],
+        name: 'setCount',
+        outputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+      }),
+      params: `[\"${count}\"]`,
+    },
+  });
+
+  const { request_key } = result.data;
+  const qrcode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+  setQrValue(qrcode);
+
+  let timerId = setInterval(async () => {
+    const getResult = await axios.get(KLIP_GET_API_URL + request_key);
+    if (getResult.data.result) {
+      console.log('result : ', JSON.stringify(getResult.data.result));
+      clearInterval(timerId);
+    }
+  }, 1000);
+};
 
 export const getAddress = async (
   setQrValue: Dispatch<SetStateAction<string>>,
@@ -26,5 +71,3 @@ export const getAddress = async (
     }
   }, 1000);
 };
-
-export const useKlip = () => {};
