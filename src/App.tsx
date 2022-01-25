8ef958710238e82756b76b352ca020baf3a5032f
yusunghyun <ysh1513@naver.com>
2022-01-25 22:58:50 +0900

    🗑️  replace qrcode library to qrcode.react

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Button } from '@mui/material';
-import { getBalance, readCount, setCount } from './api/useCaver';
-import { KAIKAS_ADDRESS } from './constants/constants.baobab';
-
-// 1 smart contract 배포 주소 파악(가져오기)
-// 2 caver.js 이용해서 스마트 컨트랙트 연동하기
-// 3 가져온 스마트 컨트랙트 실행 결과(데이터) 웹에 표현하기
+import QRCode from 'qrcode.react';
 
 const App = (): JSX.Element => {
-  useEffect(() => {
-    readCount();
-    getBalance(KAIKAS_ADDRESS);
-    setCount(3);
-  }, []);
+  const [balance, setBalance] = useState<string>('0');
 
-  return <Button variant='contained'>Hello World</Button>;
+  const onClickGetAddress = () => {};
+
+  return (
+    <>
+      <Button variant='contained' onClick={onClickGetAddress}>
+        {balance}
+      </Button>
+      <QRCode value='DEFAULT' />
+    </>
+  );
 };
 
 export default App;
